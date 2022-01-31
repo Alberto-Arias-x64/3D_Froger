f5273c1729db7b4757ecc8a6b44e7b10da2f2133
simio <yugipoi@gmail.com>
2022-01-31 16:58:02 -0500

    configurada base de datos google

--- server.js
+++ server.js
@@ -12,11 +12,30 @@ app.set('view engine', 'ejs')
 
 // setup database
 db = mysql.createConnection({
-    host: 'sql10.freesqldatabase.com',
-    user: 'sql10466091',
-    password: 'mvEbTebKRe',
-    database: 'sql10466091'
+    host: 'sql10.freemysqlhosting.net', 
+    user: 'sql10469496',
+    password: '9lCjaUWIKU',
+    database: 'sql10469496'
 })
+
+db.connect(function (error){
+  if(error){throw error}
+  else{console.log("base 1 ok")}
+})
+db.end()
+
+db2 = mysql.createConnection({
+  host: '34.135.120.74', 
+  user: 'admin_data',
+  password: 'micos64',
+  database: 'froger'
+})
+
+db2.connect(function (error){
+  if(error){throw error}
+  else{console.log("base 2 ok")}
+})
+db2.end()
 
 //rutas de la web
 app.use('/',require('./router/rutas.js'))
