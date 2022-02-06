8f9abd1e5c6448d3862fd634b1e98fb642c87f88
simio <yugipoi@gmail.com>
2022-02-05 19:59:21 -0500

    db+modelo

--- models/modelo.js
+++ models/modelo.js
@@ -1,14 +1,37 @@
 const mysql = require('mysql')
 
 db2 = mysql.createConnection({
-    host: '34.135.120.74', 
-    user: 'admin_data',
-    password: 'micos64',
-    database: 'froger'
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: 'micos64.98'
 })
 db2.connect(function (error){
     if(error){console.log(error)}
-    else{console.log("base 2 ok")}
+    else{console.log("base local ok")}
 })
 
-module.exports = db2
+    db2.query('CREATE DATABASE IF NOT EXISTS company')
+    db2.query('USE company')
+    db2.query('CREATE TABLE IF NOT EXISTS articulos(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, nombre VARCHAR(50) NOT NULL, precio INT NOT NULL, fecha DATE NOT NULL, stock INT NOT NULL, categoria VARCHAR(20) NOT NULL, descripcion VARCHAR(255) NOT NULL, imagen VARCHAR(255) NOT NULL)')
+
+    db2.query('CREATE DATABASE IF NOT EXISTS company')
+    db2.query('USE company')
+    db2.query('CREATE TABLE IF NOT EXISTS articulos2(nombre VARCHAR(50))')
+    
+    const db =new function() {
+        this.consultar_base = function() {
+            db2.query('SELECT * FROM articulos',(err,rows) =>{
+                if(!err){
+                    return(rows)
+                }
+                else{
+                    console.log(err)
+                }
+            })
+        }
+        this.print = function(){
+            console.log("ok")
+        }
+    }
+module.exports = db
--- router/rutas.js
+++ router/rutas.js
@@ -6,14 +6,7 @@ router.get('/', (req, res) => {
     res.render('index')
 })
 router.get('/2', (req, res) => {
-    db.query('SELECT * FROM articulos',(err,rows) =>{
-        if(!err){
-            res.json(rows)
-        }
-        else{
-            console.log(err)
-        }
-    })
+    res.json(db.consultar_base())
 })
 
 module.exports = router
